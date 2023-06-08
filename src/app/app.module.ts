285407d1cabd6eb7d0340f82e4f7c95485afc242
Tvisha <tvisharajiv.csbs2020@citchennai.net>
2023-06-09 00:04:53 +0530

    modify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,58 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { L1Component } from './l1/l1.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingComponent } from './landing/landing.component';
+import { CategorysearchComponent } from './categorysearch/categorysearch.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { QueryComponent } from './query/query.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProductComponent } from './product/product.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { HomeComponent } from './home/home.component';
+import { SearchbComponent } from './books/searchb/searchb.component';
+import { SearchrComponent } from './books/searchr/searchr.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    L1Component,
+    LandingComponent,
+    CategorysearchComponent,
+    LoginComponent,
+    SignupComponent,
+    QueryComponent,
+    ProfileComponent,
+    ProductComponent,
+    HomeComponent,
+    SearchbComponent,
+    SearchrComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatPaginatorModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'lmsp-88ae7',
+    appId: '1:794458909769:web:7f2073759bc68c50ebe398',
+    storageBucket: 'lmsp-88ae7.appspot.com',
+    apiKey: 'AIzaSyBpEBg1q0f_46y_eCHzWCJO-LKL0Xvi54I',
+    authDomain: 'lmsp-88ae7.firebaseapp.com',
+    messagingSenderId: '794458909769',
+    measurementId: 'G-T30XKQWE0S',
+  },
+};
